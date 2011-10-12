59bc98bd1b8f6a8533edd9d467b201121a093c01
Nikita Vasilyev <me@elv1s.ru>
2011-10-13 03:18:25 +0400

    RegExp error checking

--- chrome/options.js
+++ chrome/options.js
@@ -1,5 +1,21 @@
 'use strict';
 
+/**
+ * @param {string} className
+ * @nosideeffects
+ * @return {Element|null}
+ */
+HTMLElement.prototype.up = function(selector) {
+    var element = this;
+    while (element = element.parentNode) {
+        if ('webkitMatchesSelector' in element && element.webkitMatchesSelector(selector)) {
+            return element;
+        }
+    }
+    return null;
+};
+
+
 var rules = [];
 
 var defaultRules = [
@@ -27,13 +43,17 @@ function saveRoutes() {
  * @param {Element} button
  */
 function deleteRule(button) {
-    var rule = button.parentNode.parentNode.parentNode;
+    var rule = button.up('.rule');
+    rule.style.height = rule.clientHeight + 'px';
     rules.splice(getRuleIndex(rule), 1);
-    rule.classList.add('rule-hidden');
     setTimeout(function() {
-        rule.parentNode.removeChild(rule);
-        saveRoutes();
-    }, 300);
+        rule.classList.add('rule-animation');
+        rule.classList.add('rule-hidden');
+        setTimeout(function() {
+            rule.parentNode.removeChild(rule);
+            saveRoutes();
+        }, 200);
+    }, 0);
 }
 
 
@@ -92,7 +112,6 @@ window.onload = function() {
     function cloneDummy() {
         var clone = dummy.cloneNode(true);
         clone.removeAttribute('id');
-        clone.style.height = RULE_HEIGHT + 'px';
         return clone;
     }
 
@@ -127,28 +146,46 @@ window.onload = function() {
 
     var rules = document.getElementById('rules');
 
-    rules.oninput = rules.onchange = function() {
+    rules.oninput = rules.onchange = function(event) {
+        var element = event.target;
+        if (element.name === 'match') {
+            var row = element.up('.row');
+            try {
+                new RegExp(element.value);
+            } catch (error) {
+                row.classList.add('row-error');
+                element.nextElementSibling.textContent = error.message;
+                return;
+            }
+            row.classList.remove('row-error');
+        }
         saveRoutes();
     };
 
     rules.onmouseover = function(event) {
         if (event.target.classList.contains('delete')) {
-            event.target.parentNode.parentNode.parentNode.classList.add('rule-delete');
+            event.target.up('.rule').classList.add('rule-delete');
         }
     };
 
     rules.onmouseout = function(event) {
         if (event.target.classList.contains('delete')) {
-            event.target.parentNode.parentNode.parentNode.classList.remove('rule-delete');
+            event.target.up('.rule').classList.remove('rule-delete');
         }
     };
 
     document.getElementById('add_row').onclick = function() {
         var clone = cloneDummy();
         clone.classList.add('rule-hidden');
+        clone.classList.add('rule-animation');
         rules.appendChild(clone);
         setTimeout(function() {
+            clone.style.height = RULE_HEIGHT + 'px';
             clone.classList.remove('rule-hidden');
+            setTimeout(function() {
+                clone.removeAttribute('style');
+                clone.classList.remove('rule-animation');
+            }, 200);
         }, 0);
         saveRoutes();
     };
